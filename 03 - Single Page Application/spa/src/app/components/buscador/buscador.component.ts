31e5d3339132c467e31284703517936517d7567e
Himura Kenji <aaron.ariperto@gmail.com>
2019-10-05 22:00:23 -0300

    Buscador completo

---
+++ 03 - Single Page Application/spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+export class BuscadorComponent implements OnInit {
+  
+  heroes:Heroe[] = [];
+  termino:string;
+
+  constructor(private activate:ActivatedRoute,
+              private heroeService:HeroesService) { }
+
+  ngOnInit() {
+    this.activate.params.subscribe(params =>{
+      this.termino = params['termino'];
+      this.heroes = this.heroeService.buscarHeroe(params['termino']);
+      console.log(this.heroes);
+    });
+  }
+
+}
--- 03 - Single Page Application/spa/src/app/components/shared/navbar/navbar.component.ts
+++ 03 - Single Page Application/spa/src/app/components/shared/navbar/navbar.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  
+  
+  constructor(private router:Router  ) { }
 
   ngOnInit() {
   }
 
   buscarHeroe(termino:string){
-    console.log(termino);
-    
+    // console.log(termino);
+    if(termino.length != 0)
+      this.router.navigate(['/buscar',termino]);
+    else
+      this.router.navigate(['/heroes']);
+
   }
 
 }

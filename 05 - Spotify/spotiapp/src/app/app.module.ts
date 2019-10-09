c33ebc9a723912894cf618a3091e04d50693923a
Himura Kenji <aaron.ariperto@gmail.com>
2019-10-08 22:49:13 -0300

    05 - Imagen not found

--- 05 - Spotify/spotiapp/src/app/app.module.ts
+++ 05 - Spotify/spotiapp/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { NoimagePipe } from './pipes/noimage.pipe';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe
   ],
   imports: [
     BrowserModule,
